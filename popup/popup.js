f8ca573fe553c6259dbf2e3c980f3530a7ddb6ac
Mariam Hassib <hassib@fortiss.org>
2023-08-04 20:25:01 +0200

    The extension now shows the webpage title and the product name from Amazon

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,36 @@
 const getTitle = () => {
+  console.log("in get title");
   console.log("getTitle() = " + document.title);
+ // console.log("getTitle() = " + document.getElementById("productTitle").value);
   return document.title;
 }
 
+const getProductName = () => {
+  console.log("in get productname");
+  console.log("getProductName() = " + document.getElementsByTagName("productTitle").value);
+ // console.log("getTitle() = " + document.getElementById("productTitle").value);
+  return document.getElementById("productTitle").innerHTML;
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-  console.log("Execute Script");
+  console.log("Execute Script 1");
   chrome.scripting.executeScript({
     target: { tabId: tabs[0].id },
     func: getTitle
   }, (result) => {
-    console.log("Recv result = " + result[0].result);
-    document.getElementById("title").innerText = result[0].result;
+    console.log("Webpage Title = " + result[0].result);
+    document.getElementById("title").innerText ="Webpage Name:"+ result[0].result;
+   //document.getElementById("Title").innerText = result[0].result;
+  //  console.log(document.getElementsByName("productTitle").innerText);
   });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tabs[0].id },
+    func: getProductName
+  }, (result) => {
+    console.log("Product Title = " + result[0].result);
+    document.getElementById("productname").innerText ="Product Name:"+ result[0].result;
+  //  console.log(document.getElementsByName("productTitle").innerText);
+  });
+
 });
